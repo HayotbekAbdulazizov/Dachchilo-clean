d22fe51a7db105c183e81294982e7c6e4ca57ba1
Hayotbek Abdulazizov <hayotbekabdulazizov2005@gmail.com.com>
2023-04-02 18:25:08 +0500

    before fully replacement of try catch to errorCatcher decorator

---
+++ src/shared/utils/errorHandler.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function errorHandlerController(_target: any, _key: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (req: Request, res: Response, next: NextFunction) {
+        try {
+            const result = await originalMethod.call(this, req, res, next);
+            return result;
+        } catch (err: any) {
+            console.log(err.message)
+            return res.status(403).json(err.message)
+            // next(err);
+        }
+    };
+
+    return descriptor;
+}
+
+
+
+
+
+export function globalErrorHandler(
+    _target: any,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        try {
+            const result = await originalMethod.call(this, req, res, next);
+            return result;
+        } catch (err: any) {
+            throw new Error(err.message)
+        }
+    };
+    return descriptor;
+}
